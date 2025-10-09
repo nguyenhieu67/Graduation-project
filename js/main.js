d0bfb7c4758807cac8f00c744e63272888f9a635
nguyenhieu67 <149569751+nguyenhieu67@users.noreply.github.com>
2025-10-09 18:22:24 +0900

    add all file

--- js/main.js
+++ js/main.js
@@ -20,3 +20,22 @@ tabs.forEach((tab, index) => {
         this.classList.add("active");
     };
 });
+
+function load(selector, path) {
+    const cached = localStorage.getItem(path);
+    if (cached) {
+        $(selector).innerHTML = cached;
+    }
+
+    fetch(path)
+        .then((res) => res.text())
+        .then((html) => {
+            if (html !== cached) {
+                $(selector).innerHTML = html;
+                localStorage.setItem(path, html);
+            }
+        })
+        .finally(() => {
+            window.dispatchEvent(new Event("template-loaded"));
+        });
+}
