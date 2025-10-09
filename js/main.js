77ad91b292119497f18816208b37e75aa805a870
nguyenhieu67 <149569751+nguyenhieu67@users.noreply.github.com>
2025-10-09 18:53:19 +0900

    add javascript load header & footer, tabLine

--- js/main.js
+++ js/main.js
@@ -1,26 +1,6 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-// Tab line
-
-const tabs = $$(".tab-item");
-
-const tabActive = $(".tab-item.active");
-const line = $(".tabs .line");
-
-line.style.left = tabActive.offsetLeft + "px";
-line.style.width = tabActive.offsetWidth + "px";
-
-tabs.forEach((tab, index) => {
-    tab.onmouseenter = function () {
-        $(".tab-item.active").classList.remove("active");
-
-        line.style.left = this.offsetLeft + "px";
-        line.style.width = this.offsetWidth + "px";
-        this.classList.add("active");
-    };
-});
-
 function load(selector, path) {
     const cached = localStorage.getItem(path);
     if (cached) {
@@ -39,3 +19,26 @@ function load(selector, path) {
             window.dispatchEvent(new Event("template-loaded"));
         });
 }
+
+// Tab line
+
+setTimeout(() => {
+    const tabs = $$(".tab-item");
+
+    const tabActive = $(".tab-item.active");
+    const line = $(".tabs .line");
+    console.log(line, tabActive);
+
+    line.style.left = tabActive.offsetLeft + "px";
+    line.style.width = tabActive.offsetWidth + "px";
+
+    tabs.forEach((tab) => {
+        tab.onmouseenter = function () {
+            $(".tab-item.active").classList.remove("active");
+
+            line.style.left = this.offsetLeft + "px";
+            line.style.width = this.offsetWidth + "px";
+            this.classList.add("active");
+        };
+    });
+}, 1);
